238c400f70e9cac50bba805bddbf09a0dad18c27
Homer-Mctavish <49701255+Homer-Mctavish@users.noreply.github.com>
2022-04-28 23:05:55 -0400

    Update server.js

--- SPOC-Direct/server.js
+++ SPOC-Direct/server.js
@@ -2,18 +2,56 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
 const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 const fs = require('fs')
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
 
+app.use(session({
+    secret: "MyLittleSecretThatIdontWantOthersToKnow",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-mongoose.connect('mongodb://localhost:27017/forumDB',
+mongoose.connect('mongodb://127.0.0.1:27017/forumDB',
     {useNewUrlParser: true}, function () {
         console.log("db connection successful");
     });
 
+const userSchema=new mongoose.Schema({
+    username:{
+        type:String,
+        minlength: 3
+    },
+    password:{
+        type: String,
+    },
+    email:{
+        type: String,
+    },
+    profile:{
+        type: String,
+    }
+})
+
+userSchema.plugin(passportLocalMongoose)
+
+const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+const date = new Date()
+const year =date.getFullYear();
 
 // console.log(year)
 
